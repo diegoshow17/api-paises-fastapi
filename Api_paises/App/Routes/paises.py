bf1203ee4bce0aade41d95cbdd7d612167522478
Diego <diegodiaz0717@gmail.com>
2026-02-25 00:44:25 +0100

    Delete Api_paises/App directory

--- Api_paises/App/Routes/paises.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from app.controllers.pais_controller import get_pais
-
-router = APIRouter()
-
-@router.get("/paises/{nombre}")
-def obtener_pais(nombre: str):
-    return get_pais(nombre)
--- Api_paises/App/main.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-from app.routes.paises import router as pais_router
-
-app = FastAPI(title="API de Paises")
-
-app.include_router(pais_router, prefix="/api")
